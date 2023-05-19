c742b485b68975443d4221c0f326962dff90c1b9
Dev1 <Dev1@gmail.com>
2023-05-19 16:58:23 +0530

    adding prog.java to master file

---
+++ program/src/program/pack1/prog.java
@@ -0,0 +1,16 @@
+package program.pack1;
+
+public class prog 
+{
+	public void Meth1()
+	{
+		System.out.println("hi");
+		System.out.println("to all");
+	}
+	public static void main(String[] args)
+	{
+		new prog().Meth1();
+		
+	}
+
+}
